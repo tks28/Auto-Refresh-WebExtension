563b6ab170e875522c81fe3d8035181f60fd41c8
tks28 <tankuanshin28@gmail.com>
2022-06-29 10:37:30 +0800

    Added seconds and counter input boxes

---
+++ autoRefresh.js
@@ -0,0 +1,12 @@
+var seconds = document.getElementById("sec");
+var counter = document.getElementById("counter");
+var button = document.getElementById("start_button");
+
+var refresh = 0;
+var buttonState = false;
+
+function button_clickHandler(e) {
+    if(buttonState === false){
+        
+    }
+}
