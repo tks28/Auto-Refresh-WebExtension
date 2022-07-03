b92218b58b4c4599eb27c8583175e7cb6dcf58e4
tks28 <tankuanshin28@gmail.com>
2022-07-03 13:17:41 +0800

    Minor Changes

--- autoRefresh.js
+++ autoRefresh.js
@@ -1,12 +1,44 @@
 var seconds = document.getElementById("sec");
-var counter = document.getElementById("counter");
+var count = document.getElementById("counter");
 var button = document.getElementById("start_button");
 
 var refresh = 0;
+var counter = 0;
 var buttonState = false;
 
 function button_clickHandler(e) {
     if(buttonState === false){
-        
+        refresh = parseInt(seconds.value);
+        //counter = parseInt(count.value);
+
+        if(refresh !== 0){
+            button.style.backgroundImage = 'url("images/stop-button.png")';
+            countdown(refresh);
+        }
     }
 }
+
+function countdown(seconds) {
+    var s = seconds;
+    var x = setInterval((fucntion(){
+        setInterval(function() {
+            if(buttonState === true){
+                browser.browserAction.setBadgeText({text:''+s});
+                s--;
+
+                if(s<0){
+                    reload();
+                    s = seconds;
+                }
+            }
+            else {
+                clearInterval(x);
+                browser.browserAction.setBadgeText({text:''});
+            }
+        }, 1000);
+    })(), 0);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    button.addEventListener('click', button_clickHandler);
+});
