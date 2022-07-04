c3e97e47eeada1c1b41f3d106e2b30a639673279
tks28 <tankuanshin28@gmail.com>
2022-07-04 11:44:37 +0800

    Added comments

--- autoRefresh.js
+++ autoRefresh.js
@@ -1,16 +1,20 @@
+// variables from the html
 var seconds = document.getElementById("sec");
 var count = document.getElementById("counter");
 var button = document.getElementById("start_button");
 var counting = document.getElementById("counting");
 
+// local variables
 var refresh = 0;
 var counter = 0;
 var buttonState = false;
 
+// reload function which passes a message to background.js to execute
 function reload() {
     browser.runtime.sendMessage({message: "reload"}); 
 }
 
+// reset fucntion that resets the extension
 function reset() {
     seconds.innerHTML = 1;
     refresh = 0;
@@ -18,14 +22,18 @@ function reset() {
     browser.browserAction.setBadgeText({text:""});
 }
 
+// function when the button is clicked
 function button_clickHandler(e) {
     if(buttonState === false){
+        // get the value of seconds and counter from the input
         refresh = parseInt(seconds.value);
         counter = parseInt(count.value);
         counting.innerText = counter + "";
 
         if(refresh !== 0) {
+            // change the image of the button to stop
             button.style.backgroundImage = 'url("images/stop-button.png")';
+            // start the countdown
             countdown(refresh);
             buttonState = true;
         }
@@ -38,21 +46,27 @@ function button_clickHandler(e) {
     }
 }
 
+// countdown function
 function countdown(seconds) {
     var s = seconds;
+    // run at a interval time
     var x = setInterval((function() {
         setInterval(function() {
           if(buttonState === true && counter > 0) { 
+            // display the seconds remaining on the badge of the extension
             browser.browserAction.setBadgeText({text:"" + s});
             s--;
             if(s < 0) {
+              // if the timer reach 0 refresh the page
               reload();
               s = seconds;
+              // update the remaining refresh left
               counter--
               counting.innerText = counter + "";
             }
           }
           else { 
+            // if the counter reaches 0
             clearInterval(x);
             browser.browserAction.setBadgeText({text:""});
             alert('The extension have already refresh ' + count.value + ' times');
@@ -61,6 +75,7 @@ function countdown(seconds) {
       })(), 0);
 }
 
+// event listensers after DOM is loaded
 document.addEventListener('DOMContentLoaded', function() {
     reset();
     button.addEventListener('click', button_clickHandler);
--- background.js
+++ background.js
@@ -1,8 +1,12 @@
+// if receive the reuquest mesasge from autoRefresh.js
 browser.runtime.onMessage.addListener( function(request,sender,sendResponse) {
     if( request.message === "reload" ) {
+        // get the current active tab from the browser
         let querying = browser.tabs.query({currentWindow: true, active:true});
         querying.then(function(tab){
+            //store the code
             var code = 'window.location.reload(true);';
+            //execute the code on the current tab
             browser.tabs.executeScript(tab.id, {code: code});
         }, null);
     }
